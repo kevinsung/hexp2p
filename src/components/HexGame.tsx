652f96615519e19f99d8a0765b418df6ea7a7728
Kevin J. Sung <kevjsung@umich.edu>
2021-04-23 15:37:53 -0400

    position hexagons for hex board

--- src/components/HexGame.tsx
+++ src/components/HexGame.tsx
@@ -5,31 +5,50 @@ import { selectHexGameState } from '../slices/hexGameSlice';
 interface HexagonProps {
   translateX: number;
   translateY: number;
-  scale: number;
 }
 
 interface HexBoardProps {
   size: number;
+  scale: number;
 }
 
 function Hexagon(props: HexagonProps) {
-  const { translateX, translateY, scale } = props;
-  const transform = `translate(${translateX} ${translateY}) scale(${scale})`;
+  const { translateX, translateY } = props;
+  const transform = `translate(${translateX} ${translateY})`;
+  const d = Math.sqrt(3) / 2;
+  const points = `0,1 ${d},0.5 ${d},-0.5 0,-1 ${-d},-0.5 ${-d},0.5`;
   return (
     <polygon
-      points="0,1 0.8660254,0.5 0.8660254,-0.5 0,-1 -0.8660254,-0.5 -0.8660254,0.5"
+      points={points}
       transform={transform}
+      fill="#808080"
+      stroke="#404040"
+      strokeWidth="0.05"
     />
   );
 }
 
 function HexBoard(props: HexBoardProps) {
-  const { size } = props;
+  const { size, scale } = props;
+  const transform = `scale(${scale})`;
+  const hexagons = [];
+  const xDelta = Math.sqrt(3);
+  let xStart = 0;
+  for (let row = 0; row < size; row += 1) {
+    const translateY = 1.5 * row;
+    for (let col = 0; col < size; col += 1) {
+      const translateX = xStart + xDelta * col;
+      hexagons.push(
+        <Hexagon translateX={translateX} translateY={translateY} />
+      );
+    }
+    xStart += xDelta / 2;
+  }
   return (
     <div>
       {size}
-      <svg viewBox="0 0 100 100" width={100} height={100}>
-        <Hexagon translateX={20} translateY={20} scale={20} />
+      <svg viewBox="0 0 800 800" width={800} height={800}>
+        <g transform={transform}>{hexagons}</g>
       </svg>
     </div>
   );
@@ -38,5 +57,5 @@ function HexBoard(props: HexBoardProps) {
 export default function HexGame() {
   const { settings } = useSelector(selectHexGameState);
   const { boardSize } = settings;
-  return <HexBoard size={boardSize} />;
+  return <HexBoard size={boardSize} scale={20} />;
 }
