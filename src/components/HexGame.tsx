5478b63e7af1741fcc38e34aa5856dca05b93fdb
Kevin J. Sung <kevjsung@umich.edu>
2021-04-26 11:06:19 -0400

    split HexBoard into components

--- src/components/HexGame.tsx
+++ src/components/HexGame.tsx
@@ -11,28 +11,17 @@ import '../App.global.css';
 
 const COORDINATE_LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
-interface WinningComponentMarkerProps {
-  row: number;
-  col: number;
-}
-
 interface HexagonProps {
   row: number;
   col: number;
 }
 
 interface HexBoardProps {
-  size: number;
   winningComponent: Array<Array<number>>;
 }
 
-function WinningComponentMarker(props: WinningComponentMarkerProps) {
-  const { row, col } = props;
-  const d = 0.5 * Math.sqrt(3);
-  const translateX = (row + 1 + 2 * col) * d;
-  const translateY = 1 + 1.5 * row;
-  const transform = `translate(${translateX} ${translateY})`;
-  return <circle r="0.2" transform={transform} />;
+interface ComponentMarkerProps {
+  component: Array<Array<number>>;
 }
 
 function Hexagon(props: HexagonProps) {
@@ -102,48 +91,76 @@ function Hexagon(props: HexagonProps) {
   );
 }
 
-function HexBoard(props: HexBoardProps) {
-  const { size, winningComponent } = props;
+function Hexagons() {
+  const { settings } = useSelector(selectHexGameState);
+  const { boardSize } = settings;
   const dispatch = useDispatch();
-  const d = 0.5 * Math.sqrt(3);
-
-  const margin = 1;
-  const width = (3 * size - 1) * d + 2 * margin;
-  const height = 1.5 * size + 0.5 + 2 * margin;
-  const viewBox = `${-margin} ${-margin} ${width} ${height}`;
 
   const hexagons = [];
-  for (let row = 0; row < size; row += 1) {
-    for (let col = 0; col < size; col += 1) {
+  for (let row = 0; row < boardSize; row += 1) {
+    for (let col = 0; col < boardSize; col += 1) {
       const key = `hexagon ${row} ${col}`;
       hexagons.push(<Hexagon key={key} row={row} col={col} />);
     }
   }
+
+  const onMouseLeave = () => {
+    dispatch(hexGameStateUpdated({ selectedHexagon: [NaN, NaN] }));
+  };
+
+  return <g onMouseLeave={onMouseLeave}>{hexagons}</g>;
+}
+
+function Borders() {
+  const { settings } = useSelector(selectHexGameState);
+  const { boardSize } = settings;
+  const d = 0.5 * Math.sqrt(3);
 
   // TODO fix borders overlapping in top left and bottom right corners
   const topBorderPoints = [];
   const bottomBorderPoints = [];
   const leftBorderPoints = [];
   const rightBorderPoints = [];
-  const coordinateLabels = [];
-  bottomBorderPoints.push(`${(size - 0.5) * d},${1.5 * size + 0.25}`);
-  rightBorderPoints.push(`${(2 * size - 0.5) * d},0.25`);
-  for (let i = 0; i < size; i += 1) {
+  bottomBorderPoints.push(`${(boardSize - 0.5) * d},${1.5 * boardSize + 0.25}`);
+  rightBorderPoints.push(`${(2 * boardSize - 0.5) * d},0.25`);
+  for (let i = 0; i < boardSize; i += 1) {
     topBorderPoints.push(`${2 * i * d},0.5 ${(2 * i + 1) * d},0`);
     bottomBorderPoints.push(
-      `${(2 * i + size) * d},${1.5 * size + 0.5} ${(2 * i + size + 1) * d},${
-        1.5 * size
-      }`
+      `${(2 * i + boardSize) * d},${1.5 * boardSize + 0.5} ${
+        (2 * i + boardSize + 1) * d
+      },${1.5 * boardSize}`
     );
     leftBorderPoints.push(
       `${i * d},${1.5 * i + 0.5} ${i * d},${1.5 * i + 1.5}`
     );
     rightBorderPoints.push(
-      `${(i + 2 * size) * d},${1.5 * i + 0.5} ${(i + 2 * size) * d},${
+      `${(i + 2 * boardSize) * d},${1.5 * i + 0.5} ${(i + 2 * boardSize) * d},${
         1.5 * i + 1.5
       }`
     );
+  }
+  topBorderPoints.push(`${(2 * boardSize - 0.5) * d},0.25`);
+  leftBorderPoints.push(
+    `${(boardSize - 0.5) * d},${1.5 * (boardSize - 1) + 1.75}`
+  );
 
+  return (
+    <g className="Border">
+      <polyline points={topBorderPoints.join(' ')} stroke="#000000" />
+      <polyline points={leftBorderPoints.join(' ')} stroke="#ffffff" />
+      <polyline points={rightBorderPoints.join(' ')} stroke="#ffffff" />
+      <polyline points={bottomBorderPoints.join(' ')} stroke="#000000" />
+    </g>
+  );
+}
+
+function CoordinateLabels() {
+  const { settings } = useSelector(selectHexGameState);
+  const { boardSize } = settings;
+  const d = 0.5 * Math.sqrt(3);
+
+  const coordinateLabels = [];
+  for (let i = 0; i < boardSize; i += 1) {
     const topLabelKey = `coordinateLabel ${COORDINATE_LETTERS[i]}`;
     const leftLabelKey = `coordinateLabel ${i}`;
     const leftLabelOffset = i + 1 < 10 ? 0.7 : 0.95;
@@ -158,46 +175,54 @@ function HexBoard(props: HexBoardProps) {
       </text>
     );
   }
-  topBorderPoints.push(`${(2 * size - 0.5) * d},0.25`);
-  leftBorderPoints.push(`${(size - 0.5) * d},${1.5 * (size - 1) + 1.75}`);
 
-  const winningComponentMarkers = [];
-  for (let i = 0; i < winningComponent.length; i += 1) {
-    const [row, col] = winningComponent[i];
-    winningComponentMarkers.push(
-      <WinningComponentMarker row={row} col={col} />
-    );
+  return <g className="CoordinateLabel">{coordinateLabels}</g>;
+}
+
+function ComponentMarker(props: ComponentMarkerProps) {
+  const { component } = props;
+  const d = 0.5 * Math.sqrt(3);
+  const markers = [];
+  for (let i = 0; i < component.length; i += 1) {
+    const [row, col] = component[i];
+    const translateX = (row + 1 + 2 * col) * d;
+    const translateY = 1 + 1.5 * row;
+    const transform = `translate(${translateX} ${translateY})`;
+    markers.push(<circle r="0.2" transform={transform} />);
   }
+  return <g className="PieceMarker">{markers}</g>;
+}
 
-  const onMouseLeave = () => {
-    dispatch(hexGameStateUpdated({ selectedHexagon: [NaN, NaN] }));
-  };
+function HexBoard(props: HexBoardProps) {
+  const { winningComponent } = props;
+  const { settings } = useSelector(selectHexGameState);
+  const { boardSize } = settings;
+
+  const d = 0.5 * Math.sqrt(3);
+  const margin = 1;
+  const width = (3 * boardSize - 1) * d + 2 * margin;
+  const height = 1.5 * boardSize + 0.5 + 2 * margin;
+  const viewBox = `${-margin} ${-margin} ${width} ${height}`;
 
   return (
     <div>
       <svg className="HexBoard" viewBox={viewBox}>
-        <g onMouseLeave={onMouseLeave}>{hexagons}</g>
-        <g className="Border">
-          <polyline points={topBorderPoints.join(' ')} stroke="#000000" />
-          <polyline points={leftBorderPoints.join(' ')} stroke="#ffffff" />
-          <polyline points={rightBorderPoints.join(' ')} stroke="#ffffff" />
-          <polyline points={bottomBorderPoints.join(' ')} stroke="#000000" />
-        </g>
-        <g className="CoordinateLabel">{coordinateLabels}</g>
-        <g className="WinningComponentMarker">{winningComponentMarkers}</g>
+        <Hexagons />
+        <Borders />
+        <CoordinateLabels />
+        <ComponentMarker component={winningComponent} />
       </svg>
     </div>
   );
 }
 
 export default function HexGame() {
-  const { settings, boardState } = useSelector(selectHexGameState);
-  const { boardSize } = settings;
+  const { boardState } = useSelector(selectHexGameState);
   const winningComponent = getWinningConnectedComponent(boardState);
   return (
     <div>
       <Link to="/">Home</Link>
-      <HexBoard size={boardSize} winningComponent={winningComponent} />
+      <HexBoard winningComponent={winningComponent} />
     </div>
   );
 }
